7bca86d33a32471ac1a49c8b76de83d92dee754f
davean <davean@xkcd.com>
2020-09-26 00:28:12 +0000

    IORefU

--- smallpt-hs.hs
+++ smallpt-hs.hs
@@ -8,6 +8,7 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Storable.Mutable as VM
 import Data.List (find, minimumBy, foldl')
 import Data.IORef
+import Data.IORef.Unboxed
 import Data.Word
 import Text.Printf
 import Foreign
@@ -20,18 +21,17 @@ import Control.Concurrent
 import GHC.Float hiding (clamp)
 -- position, also color (r,g,b)
 data Vec = Vec {-# UNPACK #-} !Double {-# UNPACK #-} !Double {-# UNPACK #-} !Double
+
 zerov = Vec 0 0 0
 addv (Vec a b c) (Vec x y z) = Vec (a+x) (b+y) (c+z)
 subv (Vec a b c) (Vec x y z) = Vec (a-x) (b-y) (c-z)
-{-# INLINE subv #-}
 mulvs (Vec a b c) x = Vec (a*x) (b*x) (c*x)
 mulv (Vec a b c) (Vec x y z) = Vec (a*x) (b*y) (c*z)
 len (Vec a b c) = sqrt $ a*a+b*b+c*c
 norm v = v `mulvs` (1/len v)
 dot (Vec a b c) (Vec x y z) = a*x+b*y+c*z
-{-# INLINE dot #-}
 cross (Vec a b c) (Vec x y z) = Vec (b*z-c*y) (c*x-a*z) (a*y-b*x)
-maxv (Vec a b c) = maximum [a,b,c]
+maxv (Vec a b c) = max a (max b c)
 
 data Ray = Ray {-# UNPACK #-} !Vec {-# UNPACK #-} !Vec -- origin, direction
 
@@ -81,69 +81,69 @@ sphMirr  = Sphere 16.5 (Vec 27 16.5 47)           zerov (Vec 0.999 0.999 0.999) 
 sphGlas  = Sphere 16.5 (Vec 73 16.5 78)           zerov (Vec 0.999 0.999 0.999) REFR --Glas
 sphLite  = Sphere 600  (Vec 50 (681.6-0.27) 81.6) (Vec 12 12 12)       zerov DIFF --Lite
 
-radiance :: Ray -> CInt -> TVar Word -> IO Vec
+radiance :: Ray -> CInt -> IORefU Word -> IO Vec
 radiance ray@(Ray o d) depth xi = case intersects ray of
-  (# t, _ #) | t == (1/0.0) -> return zerov
-  (# t,Sphere r p e c refl #) -> do
-    let x = o `addv` (d `mulvs` t)
-        n = norm $ x `subv` p
-        nl = if n `dot` d < 0 then n else n `mulvs` (-1)
-        pr = maxv c
-        depth' = depth + 1
-        continue f = case refl of
+  (# !t, _ #) | t == (1/0.0) -> return zerov
+  (# !t, !Sphere r p e c refl #) -> do
+    let !x = o `addv` (d `mulvs` t)
+        !n = norm $ x `subv` p
+        !nl = if n `dot` d < 0 then n else n `mulvs` (-1)
+        !pr = maxv c
+        !depth' = depth + 1
+        continue !f = case refl of
           DIFF -> do
-            r1 <- ((2*pi)*) `fmap` erand48 xi
-            r2 <- erand48 xi
-            let r2s = sqrt r2
-                w@(Vec wx _ _) = nl
-                u = norm $ (if abs wx > 0.1 then (Vec 0 1 0) else (Vec 1 0 0)) `cross` w
-                v = w `cross` u
-                d' = norm $ (u`mulvs`(cos r1*r2s)) `addv` (v`mulvs`(sin r1*r2s)) `addv` (w`mulvs`sqrt (1-r2))
-            rad <- radiance (Ray x d') depth' xi
+            !r1 <- ((2*pi)*) `fmap` erand48 xi
+            !r2 <- erand48 xi
+            let !r2s = sqrt r2
+                !w@(Vec !wx _ _) = nl
+                !u = norm $ (if abs wx > 0.1 then (Vec 0 1 0) else (Vec 1 0 0)) `cross` w
+                !v = w `cross` u
+                !d' = norm $ (u`mulvs`(cos r1*r2s)) `addv` (v`mulvs`(sin r1*r2s)) `addv` (w`mulvs`sqrt (1-r2))
+            !rad <- radiance (Ray x d') depth' xi
             return $ e `addv` (f `mulv` rad)
 
           SPEC -> do
-            let d' = d `subv` (n `mulvs` (2 * (n`dot`d)))
-            rad <- radiance (Ray x d') depth' xi
+            let !d' = d `subv` (n `mulvs` (2 * (n`dot`d)))
+            !rad <- radiance (Ray x d') depth' xi
             return $ e `addv` (f `mulv` rad)
 
           REFR -> do
-            let reflRay = Ray x (d `subv` (n `mulvs` (2* n`dot`d))) -- Ideal dielectric REFRACTION
-                into = n`dot`nl > 0                -- Ray from outside going in?
-                nc = 1
-                nt = 1.5
-                nnt = if into then nc/nt else nt/nc
-                ddn= d`dot`nl
-                cos2t = 1-nnt*nnt*(1-ddn*ddn)
+            let !reflRay = Ray x (d `subv` (n `mulvs` (2* n`dot`d))) -- Ideal dielectric REFRACTION
+                !into = n`dot`nl > 0                -- Ray from outside going in?
+                !nc = 1
+                !nt = 1.5
+                !nnt = if into then nc/nt else nt/nc
+                !ddn= d`dot`nl
+                !cos2t = 1-nnt*nnt*(1-ddn*ddn)
             if cos2t<0    -- Total internal reflection
               then do
-                rad <- radiance reflRay depth' xi
+                !rad <- radiance reflRay depth' xi
                 return $ e `addv` (f `mulv` rad)
               else do
-                let tdir = norm $ (d`mulvs`nnt `subv` (n`mulvs`((if into then 1 else -1)*(ddn*nnt+sqrt cos2t))))
-                    a=nt-nc
-                    b=nt+nc
-                    r0=a*a/(b*b)
-                    c = 1-(if into then -ddn else tdir`dot`n)
-                    re=r0+(1-r0)*c*c*c*c*c
-                    tr=1-re
-                    pp=0.25+0.5*re
-                    rp=re/pp
-                    tp=tr/(1-pp)
-                rad <-
+                let !tdir = norm $ (d`mulvs`nnt `subv` (n`mulvs`((if into then 1 else -1)*(ddn*nnt+sqrt cos2t))))
+                    !a=nt-nc
+                    !b=nt+nc
+                    !r0=a*a/(b*b)
+                    !c = 1-(if into then -ddn else tdir`dot`n)
+                    !re=r0+(1-r0)*c*c*c*c*c
+                    !tr=1-re
+                    !pp=0.25+0.5*re
+                    !rp=re/pp
+                    !tp=tr/(1-pp)
+                !rad <-
                   if depth>2
-                    then do er <- erand48 xi
+                    then do !er <- erand48 xi
                             if er<pp -- Russian roulette
                               then (`mulvs` rp) `fmap` radiance reflRay depth' xi
                               else (`mulvs` tp) `fmap` radiance (Ray x tdir) depth' xi
-                    else do rad0 <- (`mulvs` re) `fmap` radiance reflRay depth' xi
-                            rad1 <- (`mulvs` tr) `fmap` radiance(Ray x tdir) depth' xi
+                    else do !rad0 <- (`mulvs` re) `fmap` radiance reflRay depth' xi
+                            !rad1 <- (`mulvs` tr) `fmap` radiance(Ray x tdir) depth' xi
                             return $ rad0 `addv` rad1
                 return $ e `addv` (f `mulv` rad)
 
     if depth'>5
       then do
-        er <- erand48 xi
+        !er <- erand48 xi
         if er < pr then continue $ c `mulvs` (1/pr)
                   else return e
       else continue c
@@ -156,7 +156,7 @@ smallpt w h nsamps = do
       cx = Vec (fromIntegral w * 0.5135 / fromIntegral h) 0 0
       cy = norm (cx `cross` dir) `mulvs` 0.5135
   c <- VM.replicate (w * h) zerov
-  xi <- newTVarIO 0
+  xi <- newIORefU 0
   flip mapM_ [0..h-1] $ \y -> do
         --hPrintf stderr "\rRendering (%d spp) %5.2f%%" (samps*4::Int) (100.0*fromIntegral y/(fromIntegral h-1)::Double)
    writeXi xi y
@@ -185,19 +185,23 @@ smallpt w h nsamps = do
           Vec r g b <- VM.unsafeRead c i
           hPrintf hdl "%d %d %d " (toInt r) (toInt g) (toInt b)
 
-writeXi :: TVar Word -> Int -> IO ()
-writeXi xi (I# y) = atomically $ writeTVar xi (W# (mkErand48Seed# y))
+writeXi :: IORefU Word -> Int -> IO ()
+writeXi !xi !(I# y) = writeIORefU xi (W# (mkErand48Seed# y))
+{-# INLINE writeXi #-}
 
 mkErand48Seed# :: Int# -> Word#
 mkErand48Seed# y = 
   let yw = int2Word# y; prod = (yw `timesWord#` yw `timesWord#` yw)
   in concatWord# 0## 0## prod
+{-# INLINE mkErand48Seed# #-}
 
-mkErand48Seed :: Int -> IO (TVar Word)
-mkErand48Seed (I# i) = newTVarIO (W# (mkErand48Seed# i))
+mkErand48Seed :: Int -> IO (IORefU Word)
+mkErand48Seed !(I# i) = newIORefU (W# (mkErand48Seed# i))
+{-# INLINE mkErand48Seed #-}
 
 concatWord# :: Word# -> Word# -> Word# -> Word#
 concatWord# x0 x1 x2 = x0 `or#` (x1 `uncheckedShiftL#` 16#) `or#` (x2 `uncheckedShiftL#` 32#)
+{-# INLINE concatWord# #-}
 
 -- | returns the full state and the bitmasked value.
 rand48_step# :: Word# -> (# Word#, Word# #)
@@ -209,6 +213,7 @@ rand48_step# x =
       lc2 = 0x5##
       lc3 = 0xb##
   in (# x', x' `and#` 0xffffffffffff## #)
+{-# INLINE rand48_step# #-}
 
 erand48# :: Word# -> (# Word#, Double# #)
 erand48# s =
@@ -216,13 +221,15 @@ erand48# s =
       d_word = (0x3ff0000000000000##) `or#`
                (out' `uncheckedShiftL#` 4#)
   in (# s' , (stgWord64ToDouble d_word) -## 1.0## #)
+{-# INLINE erand48# #-}
 
-erand48 :: TVar Word -> IO Double
-erand48 t =  atomically (do
-  (W# r) <- readTVar t
+erand48 :: IORefU Word -> IO Double
+erand48 !t =  do
+  (W# r) <- readIORefU t
   let (# r', d #) = erand48# r
-  writeTVar t (W# r')
-  pure (D# d))
+  writeIORefU t (W# r')
+  pure (D# d)
+{-# INLINE erand48 #-}
 
 instance Storable Vec where
   sizeOf _ = sizeOf (undefined :: Double) * 3
